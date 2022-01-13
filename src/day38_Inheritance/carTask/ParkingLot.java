568a5f34e5e459672ad25723256e45763d7ab445
CristinaC <romancristina88@yahoo.com>
2022-01-13 11:48:12 -0500

    adding objects

---
+++ src/day38_Inheritance/carTask/ParkingLot.java
@@ -0,0 +1,13 @@
+package day38_Inheritance.carTask;
+
+public class ParkingLot {
+    public static void main(String[] args) {
+        Toyota toyota= new Toyota("Camry",2020,20000,"Gray",1235);
+
+        Tesla tesla = new Tesla("Model S", 2022, 94990, "Red", 0);
+
+        BMW bmw = new BMW("X6", 2022, 67350, "White", 120000);
+
+
+    }
+}
