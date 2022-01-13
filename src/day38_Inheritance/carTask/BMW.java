cf6b0ea7b891fe7d9f64c6a6782302d701363855
CristinaC <romancristina88@yahoo.com>
2022-01-13 11:04:09 -0500

    changes made

--- src/day38_Inheritance/carTask/BMW.java
+++ src/day38_Inheritance/carTask/BMW.java
@@ -1,4 +1,15 @@
 package day38_Inheritance.carTask;
 
-public class BMW {
+public class BMW extends Car{
+    public BMW( String model, int year, double price, String color, int miles) {
+        super("BMW", model, year, price, color, miles);
+    }
+
+    public void racing(){
+        System.out.println(brand+ " "+ model+" is rasing");
+    }
+
+    public void breaksDown(){
+        System.out.println(brand+ " "+ model+" breaks down");
+    }
 }
